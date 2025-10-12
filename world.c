157fe8a45044806a5e7740ebda93ef58f4ee8dae
Nischal <nischalreddymuthumula@Nischals-MacBook-Pro.local>
2025-10-12 22:00:32 +0530

    secondcommit

---
+++ world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // The printf function outputs the string to the standard output (console).
+    printf("Hello, World!\n");
+    
+    // main() typically returns 0 to indicate successful execution.
+    return 0;
+}
